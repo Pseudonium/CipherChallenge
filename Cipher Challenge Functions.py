fce29c994ca5f0f386bfe6f86228d236b6228f6e
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2018-10-24 19:28:00 +0100

    Added prob key finder and encipher for viginere

--- Cipher Challenge Functions.py
+++ Cipher Challenge Functions.py
@@ -186,7 +186,7 @@ class Viginere:
     def __init__(self, text, key=""):
         self.text = text
         self.key = key
-        self.auto = bool(key)
+        self.auto = not bool(key)
 
     @property
     def prob_key_length(self):
@@ -202,6 +202,43 @@ class Viginere:
                 return possible_length
         else:
             return 1
+
+    @property
+    def prob_key(self):
+        text = letters(self.text).lower()
+        split_text = [
+            "".join(text[offset::self.prob_key_length])
+            for offset in range(self.prob_key_length)
+        ]
+        shifts = []
+        for split in split_text:
+            modal_char = auto_freq_analyser(split)[0].character
+            split_shift = english_chars.index("e") - (
+                english_chars.index(modal_char)
+            )
+            shifts.append(split_shift)
+        return "".join(english_chars[shift] for shift in shifts)
+
+    def encipher(self):
+        if self.auto:
+            self.key = self.prob_key
+        text = letters(self.text).lower()
+        split_text = [
+            "".join(text[offset::self.prob_key_length])
+            for offset in range(self.prob_key_length)
+        ]
+        shifted_split = list()
+        for index, split in enumerate(split_text):
+            split = Caesar(
+                split,
+                shift=ENGLISH_LANG_LEN-english_chars.index(self.key[index])
+                # Above added since vigenere keys are the complement, usually
+            )
+            shifted_split.append(split.encipher())
+        return "".join(
+            "".join(chunk)
+            for chunk in itertools.zip_longest(*shifted_split, fillvalue=" ")
+        )
 
 
 if __name__ == "__main__":
@@ -228,6 +265,10 @@ if __name__ == "__main__":
 
     BTDWV EPO IGAQ OV YJWK AMAIEH SOZ FWI KZCTY KN GHM EEMIQBURN HBMM AU XYM IMRBOAS."""
 
+    encrypted_text_6A = """Lelqzq, C xnyhv Isxad en Gkcghhe ufc wbw gem ugejldv maw efjdexq. Rly zzh vwdr fwzhcff xbw LMXSR sjwqenauim gm e qakh agnwy ugemw zvimmh Mwkgoc leeamk u dnx ix msckd evgtx fgnocff jij sly ehwmamk wzztnwq en lgi Nwltfw nj Ujsigar. Xbss kund qy lhqy ln vylqmynd mn xqsg Bnhcw'r jladrx ss xbw Avclhwb Etwyml, ababl bsr e wgkpyusmif nj ujsizsbxm xqsg lgi Nwltfw. Isxad wuqr wbw jryo vi qgtpx xhkojd sol vlyjd xi yn ryps. Xbw bpow vem am xbw kswssmifr. Xbw emlks shw vem ss xbw Fvyss Tsjzqcv, sly kdgifc en lgi zgqx ix Peclaes ogmwz vem ttmfl evie sly jtmhk nj nzd Pcygxbgtwy ss Efwwehvqmu smh nzd xbaqh if Qlivdw, uenra lgi lmhrm ge xbw Bsfgrwok. Sly Cmmazs'w wsrxfw zx Vgcvoe vem ttmfl evie sly jdquamw ix sly Ezymgkioe zx Bskmwsqrukrym, zdrww sly “yqepw” semc nj amzvxamk nzd figj. Wi skp zaui wzztnwqw qwqi zgtrx ss xbw rmnw nj ifd sz lgi Mwuih Onrxwqw ix sly smgcwmx qgqpx. Lgi ifkc lwlecfhra dngulhshk zvy lgi Mlzxow nj Twtw ul Npseomu smh nzd Luffmhy Felvdrm ge Futxpif, zrx fn-shw gem smc cvde qzdvy lgi asqhyfr qcygx bsui vwdr, mg sly gmps hkeww vi wgtpx yn ryps mm Gkcghhe. Nzd gfmd en lgi yfc sz ugejldv zaui jghrnk rxlshkbl slyjd, wcfbi faflnamk, vmkp ufc suc zvy skp mqlfidr sz Rdym. Od wbgtpx td wuxd lyjd fyuzymw Isxad'w hwsaijj luk rihl gil ssxuujilk nr ng Kshvnr vq kesamk u dnra lqecd nj zgqkyv cswmlihlr vyndefamk nzd piuzxcgm sz ugejldv zaui. Nzzx vgtkbl tw yfnyaz smgw ss fgbenw zrx vdgchgil ugejldv maw. Ay zzh vwdr nzhreamk utnyn lgi ynnpolhsh ge xbw Hqjwqmud Bmjzdvm. Lzgcltw okdh vgsl nzd Zcydryjd ehv Aiumesll bmjzdvm ogmwz zvy hnpssktbsainab zyjrmifr sz lgi Wsdwuj rlcxs, ehv zx zaqwn od emktqyv slul bluhsil khb qgtpx td ihuqcjldh nzd wued auq. Vi qwqi udlsml qmazs. Qysmabaki Dgcmy'k trypoiwldh nskihl esl xnvawqc bsr kcndr gw zr cvde. C lgmhc vi gafln td evdd xi ltvh gtv yfdqcwr sh gmi ufnxbwq fs wwtfghxcff lyj fmzl, ayn A jrio gil utvlwmx zgbym ar jcytvcff sol vlyjd sh wzvnz sly kdzyfsl qgmhyj lmazs fy."""
+
     text_1A = Affine(encrypted_text_1A)
-    text_4B = Viginere(encrypted_text_4B)
-    print(text_4B.prob_key_length)
+    text_4B = Viginere(encrypted_text_4B, key="arcanaimperii")
+    text_6A = Viginere(encrypted_text_6A, key="zeus")
+    print(text_6A.encipher())
+    print(text_4B.encipher())
